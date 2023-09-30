ffa5148ad606493a457feac8a44a0f9871af007e
gillesania20 <lorenzgillesania20@gmail.com>
2023-09-30 20:48:31 +0800

    Lesson02 finished

---
+++ Lesson02/build/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+let myName = '';
+let meaningOfLife = 0;
+let isLoading = false;
+let album = null;
+let unionType = '';
+let regex = /\w+/g;
+myName = 'Lorenz';
+meaningOfLife = 99;
+isLoading = true;
+album = undefined;
+unionType = 9;
+const sum = (a, b) => {
+    return a + b;
+};
---
+++ Lesson02/src/main.ts
@@ -0,0 +1,15 @@
+let myName: string = '';
+let meaningOfLife: number = 0;
+let isLoading: boolean = false;
+let album: any = null;
+let unionType: string | number = '';
+let regex: RegExp = /\w+/g;
+myName = 'Lorenz';
+meaningOfLife = 99;
+isLoading = true;
+album = undefined;
+unionType = 9;
+
+const sum = (a: number, b: number) => {
+    return a + b;
+}
